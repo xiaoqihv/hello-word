ab15d57bb8d514de7031f4d97ada5a19b39bee87
xiaoqihv <38977304+xiaoqihv@users.noreply.github.com>
2021-03-10 22:34:26 +0800

    Update c.php

--- c.php
+++ c.php
@@ -14,34 +14,3 @@ class_b(new class{
         return "mochu";
     }
 });
-
-
-
-
-die();
-
-
-// $content = '据披露，英国1973年的一部《公司法》草案曾引发王室高度重视：该草案中含有一项透明性条款，原本是为防止投资人暗中收购、增持公司股份，致使公司商业风险增加。一旦成为法律，英国公众将有机会获悉特定投资人或持股人的真实身份。《卫报》推测，当年女王很可能担忧该草案能让公众窥探到自己的财务状况，于是委派了私人律师去秘密游说政府相关人员。';
-// var_dump(ycl($content));
-/*
-
-deleteDir('zb');
-function deleteDir($dir){
-    if (!$handle = @opendir($dir)) {
-        return false;
-    }
-    while (false !== ($file = readdir($handle))) {
-        if ($file !== "." && $file !== "..") {
-            $file = $dir . '/' . $file;
-            if (is_dir($file)) {
-                deleteDir($file);
-            } else {
-                @unlink($file);
-            }
-        }
-
-    }
-    @rmdir($dir);
-}
-
-*/
